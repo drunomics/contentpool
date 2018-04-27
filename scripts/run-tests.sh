2eb90e3bd72319b4096604a3195b13a47db2cd16
fago <fago@wolfgangziegler.net>
2018-04-27 11:10:53 +0200

    Ensure environment variables are loaded for the test runner.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 cd `dirname $0`/../../contentpool-project/
+source dotenv/loader.sh
 
+set -x
 # Launch tests inside a docker container, so name resolution works thanks to
 # docker host aliases and the PHP environment is controlled by the container.
 docker-compose exec web ./web/profiles/contrib/contentpool/tests/behat/run.sh
