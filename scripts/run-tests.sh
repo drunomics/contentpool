68442be2ef63111c4561578e062662cf689a2b8d
fago <fago@wolfgangziegler.net>
2018-04-30 16:28:32 +0200

    Ensure dev depdencies are there.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,6 +3,11 @@ set -e
 cd `dirname $0`/../../contentpool-project/
 source dotenv/loader.sh
 
+# Ensure dev dependencies are there.
+# @todo: This should not be necessary. Remove once phapp build correctly adds
+# in dev dependencies.
+composer install
+
 set -x
 # Launch tests inside a docker container, so name resolution works thanks to
 # docker host aliases and the PHP environment is controlled by the container.
