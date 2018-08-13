4d93190a69c36ded7ba8fad731f03fa4bcd06c07
fago <fago@wolfgangziegler.net>
2018-08-13 16:47:39 +0200

    Undo rename of feature context.

---
+++ tests/behat/behat-features/src/FeatureContext.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * The main behat context.
+ */
+
+use Drupal\DrupalExtension\Context\DrupalContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends DrupalContext {
+
+}
