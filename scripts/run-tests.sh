0f648c41d049fdeda5ebf0816b59c136381c6f77
fago <fago@wolfgangziegler.net>
2018-08-13 14:58:18 +0200

    Update setup to work with localdev.space and recent drupal project.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -9,7 +9,12 @@ set -x
 PHPCS=$(readlink -f vendor/bin/phpcs)
 ( cd ./web/profiles/contrib/contentpool && $PHPCS --colors --report-width=130 )
 
+# Start chrome container.
+docker-compose -f devsetup-docker/service-chrome.yml up -d
 
 # Launch tests inside a docker container, so name resolution works thanks to
 # docker host aliases and the PHP environment is controlled by the container.
 docker-compose exec web ./web/profiles/contrib/contentpool/tests/behat/run.sh
+
+# Stop chrome container.
+docker-compose -f devsetup-docker/service-chrome.yml stop
