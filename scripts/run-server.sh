7c1ab5043a313ac79e3200b44410008d6fef9140
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2019-05-13 18:05:55 +0200

    DEV-1489: Wait for mysql to be up.

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -8,3 +8,4 @@ cd `dirname $0`/..
 cd ../contentpool-project/
 echo "Running server..."
 docker-compose up -d --build
+echo "Waiting for mysql to come up..." && docker-compose exec cli /bin/bash -c "while ! echo exit | nc mariadb 3306; do sleep 1; done" >/dev/null
