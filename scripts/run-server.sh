f8b239f5c6d4688048f36dca48e63ab821335717
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2019-05-23 14:17:59 +0200

    Add php service to traefik network.

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -9,3 +9,6 @@ cd ../contentpool-project/
 echo "Running server..."
 docker-compose up -d --build
 echo "Waiting for mysql to come up..." && docker-compose exec cli /bin/bash -c "while ! echo exit | nc mariadb 3306; do sleep 1; done" >/dev/null
+
+# Make the php service join the traefik network - this is needed so the satellite can be reached when pushing content.
+docker network connect traefik contentpool-project_php_1
