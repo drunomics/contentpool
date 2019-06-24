075782c6e1bc28efb104cb58bd44ac737cf1f2e7
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2019-06-24 19:36:43 +0200

    Compute custom elements teaser.

--- modules/contentpool_custom_elements/src/CustomElementTeaserFeldItemList.php
+++ modules/contentpool_custom_elements/src/CustomElementTeaserFeldItemList.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\contentpool_custom_elements;
 
+use drunomics\ServiceUtils\Core\Render\RendererTrait;
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
+use Drupal\custom_elements\CustomElementGeneratorTrait;
 
 /**
  * Computes a field item list for the teaser field.
@@ -12,15 +15,28 @@ use Drupal\Core\TypedData\ComputedItemListTrait;
  */
 class CustomElementTeaserFeldItemList extends FieldItemList {
 
+  use CustomElementGeneratorTrait;
   use ComputedItemListTrait;
+  use RendererTrait;
 
   /**
    * Computes the values for an item list.
    */
   protected function computeValue() {
-    $new_list = [];
-    $new_list[0] = $this->createItem(0, ['markup' => '<p>test</p>']);
-    $this->list = $new_list;
+    $node = $this->getEntity();
+    $this->list = [];
+    $build['#theme'] = 'custom_element';
+    $build['#entity_type_id'] = $node->getEntityTypeId();
+    $build['#' . $node->getEntityTypeId()] = $node;
+    $build['#view_mode'] = 'teaser';
+    $build['#custom_element'] = $this->getCustomElementGenerator()
+      ->generate($node, 'teaser');
+
+    $markup = (string) $this->getrenderer()->renderPlain($build);
+    // Make sure URLs to images etc. are all absolute, as in RSS feeds.
+    $markup = Html::transformRootRelativeUrlsToAbsolute($markup, \Drupal::request()->getSchemeAndHttpHost());
+
+    $this->list = [0 => $this->createItem(0, ['markup' => $markup])];
   }
 
 }
