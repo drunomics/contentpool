6da8d57c7a2fc38409210c3b7e5505a3231a8172
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2019-08-22 14:38:26 +0200

    WV-3085: Support CDNs with absolute URLs.

--- modules/contentpool_custom_elements/src/Component/Utility/ContentpoolHtml.php
+++ modules/contentpool_custom_elements/src/Component/Utility/ContentpoolHtml.php
@@ -3,26 +3,72 @@
 namespace Drupal\contentpool_custom_elements\Component\Utility;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Site\Settings;
 
 /**
  * Provides DOMDocument helpers for parsing and serializing HTML strings.
  *
- * @ingroup utility
+ * @see Drupal\Component\Utility\Html
  */
-class ContentpoolHtml extends Html {
+class ContentpoolHtml {
 
   /**
    * {@inheritdoc}
+   *
+   * Only cover urls pointing to files.
    */
   protected static $uriAttributes = [
     'poster',
     'src',
-    'cite',
-    'data',
-    'action',
-    'formaction',
-    'srcset',
-    'about',
   ];
+
+  /**
+   * Gets the base URL to use for files.
+   *
+   * @return string
+   *   The base URL with a trailing slash.
+   */
+  public static function getFilesBaseUrl() {
+    return Settings::get('file_public_base_url') ?: \Drupal::request()->getSchemeAndHttpHost();
+  }
+
+  /**
+   * Converts root-relative image src URLs to absolute URLs.
+   *
+   * @param string $html
+   *   The partial (X)HTML snippet to load. Invalid markup will be corrected on
+   *   import.
+   * @param string $base_url
+   *   (optional) The base URL to add.
+   *
+   * @return string
+   *   The updated (X)HTML snippet.
+   */
+  public static function transformRootRelativeUrlsToAbsolute($html, $base_url = NULL) {
+    $html_dom = Html::load($html);
+    $base_url = $base_url ?: static::getFilesBaseUrl();
+    $xpath = new \DOMXpath($html_dom);
+
+    // Update all root-relative URLs to absolute URLs in the given HTML.
+    foreach (static::$uriAttributes as $attr) {
+      foreach ($xpath->query("//*[starts-with(@$attr, '/') and not(starts-with(@$attr, 'http'))]") as $node) {
+        $node->setAttribute($attr, $base_url . $node->getAttribute($attr));
+      }
+      foreach ($xpath->query("//*[@srcset]") as $node) {
+        // @see https://html.spec.whatwg.org/multipage/embedded-content.html#attr-img-srcset
+        // @see https://html.spec.whatwg.org/multipage/embedded-content.html#image-candidate-string
+        $image_candidate_strings = explode(',', $node->getAttribute('srcset'));
+        $image_candidate_strings = array_map('trim', $image_candidate_strings);
+        for ($i = 0; $i < count($image_candidate_strings); $i++) {
+          $image_candidate_string = $image_candidate_strings[$i];
+          if ($image_candidate_string[0] === '/' && strpos($image_candidate_string, 'http') !== 0) {
+            $image_candidate_strings[$i] = $base_url . $image_candidate_string;
+          }
+        }
+        $node->setAttribute('srcset', implode(', ', $image_candidate_strings));
+      }
+    }
+    return Html::serialize($html_dom);
+  }
 
 }
